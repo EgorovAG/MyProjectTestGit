c37111a42af385edb81b0752ea35474c09f7965b
Alexandr Egorov <ealex5@mail.ru>
2020-03-30 14:42:13 +0200

    AuthFilter correct

--- web/src/main/java/com/talwin/filter/AuthFilter.java
+++ web/src/main/java/com/talwin/filter/AuthFilter.java
@@ -1,32 +1,32 @@
-//package com.talwin.filter;
-//
-//
-//import javax.servlet.*;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import java.io.IOException;
-//
+package com.talwin.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
 //@WebServlet("/student")
-//public class AuthFilter implements Filter {
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest req = (HttpServletRequest) servletRequest;
-//        Object authUser = req.getSession().getAttribute("authUser");
-//        if(authUser == null) {
-//            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/login.jsp");
-//            requestDispatcher.forward(servletRequest,servletResponse);
-//        }
-//
-//
-//    }
-//
-//    @Override
-//    public void destroy() {
-//
-//    }
-//}
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        Object authUser = req.getSession().getAttribute("authUser");
+        if(authUser == null) {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/login.jsp");
+            requestDispatcher.forward(servletRequest,servletResponse);
+        }
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
