109efafae8daac6de5f96ecf19de4eca47d8986d
Alexandr Egorov <ealex5@mail.ru>
2020-03-30 14:35:51 +0200

    correct test

--- model/src/test/java/com/talwin/salary/SummaTest.java
+++ model/src/test/java/com/talwin/salary/SummaTest.java
@@ -1,9 +1,6 @@
 package com.talwin.salary;
 
-
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
